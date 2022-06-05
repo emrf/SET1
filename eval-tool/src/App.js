06751d66cdba146c8e2bd12c9d61af093b6c233c
Samuel Meisner <sm2388@cornell.edu>
2022-06-05 14:09:21 -0400

    added interactive radar chart

--- eval-tool/src/App.js
+++ eval-tool/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import HomePage from './HomePage.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'
+// import { useState } from "react";
+// import RadarChart from "./components/RadarChart";
+// import { Data } from "./Data";
 
 function App() {
+
+
+
   return (
     <div className="App">
+      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
       <header className="App-header">
         <h1>Startup Evaluation Tool</h1>
         <HomePage />
+        {/* <div id="radar-canvas" style={{ width: 500 }}>
+          <RadarChart chartData={Data} />
+
+        </div> */}
       </header>
+
+
     </div>
+
   );
 }
 
--- eval-tool/src/HomePage.jsx
+++ eval-tool/src/HomePage.jsx
@@ -1,65 +1,93 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 import ReactSlider from 'react-slider'
-
+// import RadarChart from "./components/RadarChart";
+import { Data } from "./Data";
+import $ from 'jquery';
+import { Chart } from 'chart.js';
 
 
 export default class HomePage extends Component {
   constructor() {
     super();
+    document.title = "Startup Evaluator";
     this.state = {
       funding: "A",
       sector: "Other",
       execExper: 5,
       score: 0,
+      data: Data,
     }
     this.fundingMap = { "A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5 }
     this.sectorMap = {
-      "Ecommerce": 0, "Finance": 1,
-      "Healthcare": 2, "Insurance": 3,
-      "Tech (hardware)": 4, "Tech (software)": 5,
+      "Ecommerce": 0, "Finance": 2,
+      "Healthcare": 4, "Insurance": 6,
+      "Tech (hardware)": 8, "Tech (software)": 10,
       "Other": 0
     }
     this.fundingInput = this.fundingInput.bind(this);
     this.sectorInput = this.sectorInput.bind(this);
     this.updateSlider = this.updateSlider.bind(this);
-    this.updateResult = this.updateResult.bind(this);
-
+    this.updateScore = this.updateScore.bind(this);
+    this.updateChart = this.updateChart.bind(this);
   }
 
   updateSlider(val, i) {
-    console.log(val)
-    this.setState({ execExper: val }, this.updateResult);
+    let newData = this.state.data
+    newData.data.datasets[0].data[0] = val * 10 //set first index of "your company" dataset to exec exper val
+    this.setState({ execExper: val, data: newData }, this.updateScore);
+    this.updateChart(newData);
   }
 
   fundingInput() {
     let fundingStage = d3.select('#fundingSelect').property('value');
-    this.setState({ funding: fundingStage }, this.updateResult)
+    this.setState({ funding: fundingStage }, this.updateScore)
     console.log(fundingStage);
   }
 
   sectorInput() {
     let sector = d3.select('#sectorSelect').property('value');
-    this.setState({ sector: sector }, this.updateResult)
+    let newData = this.state.data;
+    newData.data.datasets[0].data[4] = this.sectorMap[sector] * 10;
+    this.setState({ sector: sector, data: newData }, this.updateScore)
     console.log(sector);
   }
 
-  updateResult() {
+  updateScore() {
     let fundingStage = this.state.funding;
     let fundingScore = this.fundingMap[fundingStage];
     let sector = this.state.sector;
     let sectorScore = this.sectorMap[sector];
     let execScore = this.state.execExper;
     let newScore = sectorScore + fundingScore + execScore;
+
+    let newData = this.state.data;
+    this.updateChart(newData);
+
     this.setState({ score: newScore })
     document.getElementById("score-text").innerHTML = newScore;
     console.log(newScore);
   }
 
+  updateChart(newData) {
+    var ctx = document.getElementById('radar-canvas');
+    var newChart = new Chart(ctx, newData);
+    newChart.update();
+  }
+
+  componentDidMount() {
+    var ctx = document.getElementById('radar-canvas');
+    var newChart = new Chart(ctx, this.state.data);
+    newChart.update();
+  }
 
   render() {
     return (
       <div className="Home-Page">
+        <script src="https://cdn.anychart.com/releases/8.7.1/js/anychart-core.min.js">     </script>
+        <script src="https://cdn.anychart.com/releases/8.7.1/js/anychart-radar.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+
         <br />
         <div class="QA">
           <label htmlFor="fundingSelect">
@@ -113,6 +141,11 @@ export default class HomePage extends Component {
         <div class="div-score">
           <p >Score: <span id="score-text"></span></p>
         </div>
+        <canvas id="radar-canvas" style={{ width: 500 }}>
+          {/* <RadarChart id="chart" chartData={this.state.data} /> */}
+
+        </canvas>
+
       </div >
     )
   }
