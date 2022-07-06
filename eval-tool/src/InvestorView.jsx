14f22a432c6c8ce3f80064acad28f204ceda998c
Samuel Meisner <sm2388@cornell.edu>
2022-07-06 10:03:33 -0400

    created outline for gsheet control

--- eval-tool/src/InvestorView.jsx
+++ eval-tool/src/InvestorView.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactSlider from 'react-slider'
 import axios from 'axios';
 
 const dataURL = 'https://sheet.best/api/sheets/19286fe5-7607-411d-b58f-86fc9f764b63';
@@ -7,16 +8,21 @@ const dataURL = 'https://sheet.best/api/sheets/19286fe5-7607-411d-b58f-86fc9f764
 export default class InvestorView extends Component {
   constructor() {
     super();
-    this.state = { "data": {} };
+    this.state = {};
+    this.sliderAction = this.sliderAction.bind(this);
+  }
+
+  sliderAction(val, i) {
+    this.setState({ "subjective1": val })
   }
 
   async componentDidMount() {
-    this.state.data = await axios.get(dataURL);
-    this.state.data = this.state.data.data;
-    console.log(this.state.data);
+    var tdata = await axios.get(dataURL);
+    this.state = tdata.data;
+    console.log(this.state);
     var col = [];
-    for (var i = 0; i < this.state.data.length; i++) {
-      for (var key in this.state.data[i]) {
+    for (var i = 0; i < this.state.length; i++) {
+      for (var key in this.state[i]) {
         if (col.indexOf(key) === -1) {
           col.push(key);
         }
@@ -32,11 +38,11 @@ export default class InvestorView extends Component {
 
     }
 
-    for (var i = 0; i < this.state.data.length; i++) {
+    for (var i = 0; i < this.state.length; i++) {
       tr = table.insertRow(-1);
       for (var j = 0; j < col.length; j++) {
         var tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = this.state.data[i][col[j]];
+        tabCell.innerHTML = this.state[i][col[j]];
       }
     }
 
@@ -50,8 +56,23 @@ export default class InvestorView extends Component {
     return (
       <div>
         <div id="showData">
-
         </div>
+        <div class="slider-space QA">
+          <label>Rate Team: </label>
+          <ReactSlider id="slider"
+            className="horizontal-slider"
+            marks={true}
+            defaultValue={0}
+            min={0}
+            max={100}
+            thumbClassName="example-thumb"
+            thumbActiveClassName='current-thumb'
+            trackClassName="example-track"
+            markClassName="example-mark"
+            onChange={this.sliderAction}
+            renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+            renderTrack={(props, state) => <div {...props} />}
+          /></div>
       </div>
     )
   }
